0b1530c3e7c519649ccf6eb57409d4789f155c50
Bilal <bilal.khalid@generalassemb.ly>
2019-03-16 14:18:39 -0400

    first commit, made some changes to the readme.md

---
+++ comments.py
@@ -0,0 +1,12 @@
+# test
+print('hello world') # this is a comment
+
+"""
+test 2
+"""
+
+print("Test2")
+
+print("identify home adress")
+print("identify current location in relation to destination")
+print("check for state of \t test")
