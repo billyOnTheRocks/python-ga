1080e6cb8400aa9fb15350193a97f4b62ce42e99
Bilal <bilal.khalid@generalassemb.ly>
2019-04-02 16:44:16 -0400

    starting unit 2ex

--- unit2/unit2ex.py
+++ unit2/unit2ex.py
@@ -1,18 +1,21 @@
 grocery_item = []
 item_price = []
-total = 0
+
 
 while True:
     print("========== Welcome to cvs ==========")
     print("Please scan the item and place it in your bag")
 
+    item = str(input("add your item"))
+    price = float(input("item price"))
+    item_price.append(price)
+    grocery_item.append(item)
+
+
     choice = str(input("would you like to add another item?"))
 
     if choice == 'y':
-            item = str(input("add your item"))
-            price = float(input("item price"))
-            item_price.append(price)
-            grocery_item.append(item)
+            total = 0
             total += price
             print(total)
     elif choice == 'n':
